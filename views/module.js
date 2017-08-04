5538e221436c6d4a9ab8031673657f1fa2954ded
her_username <vilas.kumkar@extrapreneursindia.com>
2017-08-04 19:00:08 +0530

    implemented routing

--- views/module.js
+++ views/module.js
@@ -18,20 +18,49 @@ app.controller('ctrl',['$scope','$modal',function ($scope,$modal) {
 
 }]);
 
+app
+	.run(function($rootScope, $state) {
+            $rootScope.$on('$stateChangeError', function(evt, to, toParams, from, fromParams, error) {
+          if (error.redirectTo) {
+            $state.go(error.redirectTo);
+          } else {
+            $state.go('error', {status: error.status})
+          }
+        })
+    })
+	.factory('userService', userService)
+	.config(routes);
 
-app.config(function ($stateProvider) {
+function userService() {
+	var usersMock = {
+    	'testUser': {
+        	username: 'testUser',
+            password: '1234'
+        },
+        'testUser2': {
+        	username: 'testUser2',
+            password: '1234'
+        }
+    };
+	var userService = {
+    	user: undefined,
+    	login: function(userCredentials) {
+        	// later --> $http.post('auth', userCredentials).then(...)
+            // for demo use local data
+            var user = usersMock[userCredentials.username]
+            userService.user = ( user && ( user.password == userCredentials.password ) ) ? 
+            	user : undefined;
+            return user;
+        },
+        logout: function() {
+        	userService.user = undefined;
+        }
+    }
+    
+    return userService;
+}
+function routes($urlRouterProvider, $stateProvider) {
+
 	
-	$stateProvider
-           .state('core', {
-                url: '/zone/',
-                views: {
-                    "header": {
-                        template: '<h1>Heyyyyy</h1>'
-                    },
-                    "": {
-                        template: '<ui-view></ui-view>'
-                    }
-                }
-            })
-})
+}
 
--- views/route.js
+++ views/route.js
@@ -1,2 +1,47 @@
 'use strict';
-angular.module('app')
+var app = angular.module('app')
+.config(['$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+      $urlRouterProvider.otherwise("/?page=a");
+      $stateProvider
+        .state('root', {
+          abstract: true,
+          views: {
+          	'header@': {
+              templateUrl: '/header/header.html'
+            },
+            'footer@': {
+              templateUrl:'/header/footer.html'
+            },
+            "":{
+            	template:'<ui-view></ui-view>'
+            }
+          },
+        })
+        .state('dashboard', {
+          parent: 'root',
+          url: '/',
+ 		  template: '',
+          controller: function($state) {
+             $state.go('root.student');
+          }
+        })
+        .state('root.student', {
+          url: '/student',
+          templateUrl: '/MENU/menu.html'
+   
+        })
+        .state('root.profile', {
+          url: '/profile',
+          template: '<h1> ==== profile ====</h1><hr/>'
+  
+        })
+        .state('root.app.error', {
+          views: {
+            'body@root': {
+              template: '<div class="error">ERROR</div>'
+            }
+          }
+        });
+    }
+  ]);
