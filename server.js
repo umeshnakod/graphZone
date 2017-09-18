726a3443e05b88faa736857331c631bc8f3efac0
Vilas Kumkar <umeshnakod@gmail.com>
2017-09-18 14:44:42 +0530

    changes

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get('/',function (req,res) {
 })
 
 
-var port = process.env.PORT = || 5000;
+var port = process.env.PORT || 5000;
 
 app.listen(port,function () {
 	console.log('server running');
